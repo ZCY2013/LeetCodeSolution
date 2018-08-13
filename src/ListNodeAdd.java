cddf195310e83ac9d7bb553f8f8b7325f3cb7e8d
ZCY2013 <zcy32897629@126.com>
2018-08-13 20:33:36 +0800

    add two List node add solution

--- src/ListNodeAdd.java
+++ src/ListNodeAdd.java
@@ -8,19 +8,37 @@
  */
 public class ListNodeAdd {
     public static void main(String[] args) {
-        ListNode list = new ListNode(2);
-        list.next = new ListNode(4);
-        list.next.next = new ListNode(3);
+        ListNode list = new ListNode(5);
+//        list.next = new ListNode(4);
+//        list.next.next = new ListNode(3);
 
         ListNode list2 = new ListNode(5);
-        list2.next = new ListNode(6);
-        list2.next.next = new ListNode(4);
+//        list2.next = new ListNode(6);
+//        list2.next.next = new ListNode(4);
 
         ListNodeAdd listadd = new ListNodeAdd();
-        listadd.addTwoNumbers(list, list2).toString();
+        System.out.println(listadd.addTwoNumbers(list, list2).toString());
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        int carry = 0;
+        while(p!=null || q!=null)
+        {
+            int a = p != null? p.val :0;
+            int b = q != null? q.val :0;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            if(p != null) p = p.next;
+            if(q != null) q = q.next;
+            curr = curr.next;
+        }
+        if(carry > 0) curr.next = new ListNode(carry);
+        return head.next;
+
     }
 }
 
