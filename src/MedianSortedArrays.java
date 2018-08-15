375d99a1285e628df2d4c32012c681901304ae85
ZCY2013 <zcy32897629@126.com>
2018-08-15 20:46:54 +0800

    init Find tow sortedArray median number method

---
+++ src/MedianSortedArrays.java
@@ -0,0 +1,15 @@
+public class MedianSortedArrays {
+    public static void main(String[] args) {
+        MedianSortedArrays bean = new MedianSortedArrays();
+        int[] nums1 = {1,3};
+        int[] nums2 = {2};
+        System.out.println(bean.findMedianSortedArrays(nums1, nums2));
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        return 0;
+    }
+}
