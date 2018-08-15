74318d7610910228423298956ed86f60a2bf9f12
ZCY2013 <zcy32897629@126.com>
2018-08-15 20:30:56 +0800

    update solution for better slide window

--- src/NoRepetitionSubstring.java
+++ src/NoRepetitionSubstring.java
@@ -3,18 +3,31 @@ import java.util.HashMap;
 public class NoRepetitionSubstring {
     public static void main(String[] args) {
         NoRepetitionSubstring bean = new NoRepetitionSubstring();
-        String s = "abcabcbb";
+        String s = " ";
         int a = bean.lengthOfLongestSubstring(s);
         System.out.println(a);
     }
+
+    /**
+     * for the longest Substring without no repetition
+     * init string : 1 2 3 4 5 6 7 8 9 10 11...
+     * start curson  *
+     * if 3 == 1
+     * while startcursor move to 3 as bellow:
+     *                   *
+     * so the length is the end-cursor substract start-cursor
+     */
     public int lengthOfLongestSubstring(String s) {
         int length = s.length();
         HashMap<Character,Integer> map = new HashMap<>();
-        int max = 0;
-        for(int i =0; i < length; i++){
-            char a = s.charAt(i);
-            if(map.containsKey(a)) max = i - map.get(a);
-            else map.put(a,i);
+        int max = 0 ;
+        int start = 0;
+        for(int i = 0; i < length; i++)
+        {
+            Character character = s.charAt(i);
+            if(map.containsKey(character))  start = Math.max(map.get(character), start);
+            max = Math.max(i - start + 1, max);
+            map.put(character , i + 1);
         }
         return max;
     }
